b5159c587eb0c8ef7265224a7e39ab9fd1950e72
Vlad Klunduk <klundukvlad40@gmail.com>
2023-10-14 22:47:45 +0200

    editing and deleting rows

--- To Do List 2.1/View/ViewController.swift
+++ To Do List 2.1/View/ViewController.swift
@@ -28,17 +28,47 @@ class ViewController: UITableViewController {
         return cell
     }
 
-    @objc func addTapped(){
+    @objc private func addTapped(){
         let ac = UIAlertController(title: "New Item", message: nil, preferredStyle: .alert)
         ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Cancel", style: .default))
         ac.addAction(UIAlertAction(title: "Save", style: .default){ [weak self] _ in
-            guard let name = ac.textFields![0].text else { return }
+            guard let name = ac.textFields![0].text, !name.isEmpty else { return }
             self?.viewModel.createItem(name)
             DispatchQueue.main.async {
                 self?.tableView.reloadData()
             }
         })
+        present(ac, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let swipe = UISwipeActionsConfiguration(actions: [UIContextualAction(style: .destructive, title: "Delete", handler: { [weak self] _, _, _ in
+            self?.deleteRow(indexPath: indexPath)
+        }),UIContextualAction(style: .normal, title: "Edit", handler: { [weak self] _, _, _ in
+            self?.editRow(indexPath: indexPath)
+        })])
+        return swipe
+    }
+    
+    func deleteRow(indexPath: IndexPath) {
+        let item = viewModel.getItems()[indexPath.row]
+        viewModel.deleteItem(item)
+        tableView.deleteRows(at: [indexPath], with: .right)
+    }
+    
+    func editRow(indexPath: IndexPath) {
+        let item = viewModel.getItems()[indexPath.row]
+        let ac = UIAlertController(title: "Change Name", message: nil, preferredStyle: .alert)
+        ac.addTextField()
         ac.addAction(UIAlertAction(title: "Cancel", style: .default))
+        ac.addAction(UIAlertAction(title: "Save", style: .default){ [weak self] _ in
+            guard let name = ac.textFields![0].text, !name.isEmpty else { return }
+            self?.viewModel.updateItem(item, newName: name)
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        })
         present(ac, animated: true)
     }
 }
